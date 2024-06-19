a67076e8b0d220b9275be3e53017001b16dc5abe
gbredz1 <10490327+gbredz1@users.noreply.github.com>
2024-06-19 12:06:56 +0200

    create crawler class & reading environment values in main

--- apps/src/worker.py
+++ apps/src/worker.py
@@ -23,23 +23,24 @@ class Worker:
     db_bucket: str
     db_org: str
 
-    def __init__(self):
-        self.mq_client = MQClient(host=os.environ.get("MQ_HOST", "localhost"))
-
+    def __init__(
+        self,
+        mq_host: str,
+        tsdb_host: str,
+        tsdb_token: str,
+        tsdb_org: str,
+        tsdb_bucket: str,
+        tsdb_debug: bool = False,
+    ):
+        self.mq_client = MQClient(host=mq_host)
         self.db_client = influxdb_client.InfluxDBClient(
-            url=os.environ.get("TSDB_HOST", "http://localhost:8086"),
-            token=os.environ.get(
-                "DOCKER_INFLUXDB_INIT_ADMIN_TOKEN", "my-super-secret-auth-token"
-            ),
-            org=os.environ.get("DOCKER_INFLUXDB_INIT_ORG", "my-org"),
-            debug=TypeAdapter(bool).validate_python(
-                os.environ.get("TSDB_DEBUG", "0")
-            ),
+            url=tsdb_host,
+            token=tsdb_token,
+            org=tsdb_org,
+            debug=tsdb_debug,
         )
-        self.db_bucket = os.environ.get(
-            "DOCKER_INFLUXDB_INIT_BUCKET", "my-bucket"
-        )
-        self.db_org = os.environ.get("DOCKER_INFLUXDB_INIT_ORG", "my-org")
+        self.db_bucket = tsdb_bucket
+        self.db_org = tsdb_org
 
     def start_consuming(self, queue_name: str):
         """start consuming message from the queue : blocking method"""
@@ -55,20 +56,20 @@ class Worker:
     ):
         try:
             message = TypeAdapter(MessageProgram).validate_json(body)
-            logger.debug(" [MQ] Received #{}", message.slug)
+            logger.debug("[MQ] Received #{}", message.slug)
 
             self.__store_message(message)
 
-            logger.debug(" [MQ] Ack #{}", message.slug)
+            logger.debug("[MQ] Ack #{}", message.slug)
             channel.basic_ack(delivery_tag=method.delivery_tag)
 
         except ValidationError:
-            logger.error(" [MQ] NAck #{}", body.decode())
+            logger.error("[MQ] NAck #{}", body.decode())
             channel.basic_nack(delivery_tag=method.delivery_tag, requeue=False)
 
         except exceptions.NewConnectionError as e:
             logger.error("[DB] connection error: {}", e)
-            logger.debug(" [MQ] NAck #{}", message.slug)
+            logger.debug("[MQ] NAck #{}", message.slug)
             channel.basic_nack(delivery_tag=method.delivery_tag, requeue=True)
             time.sleep(5)
 
@@ -88,10 +89,28 @@ def main():
     """main"""
     setup_logger()
 
-    queue_name = os.environ.get("MQ_QUEUE_NAME", "crawler")
+    logger.debug("Read env value")
+    queue_name = os.environ.get("MQ_QUEUE_NAME", "my-queue")
+    mq_host = os.environ.get("MQ_HOST", "localhost")
 
+    tsdb_host = os.environ.get("TSDB_HOST", "http://localhost:8086")
+    tsdb_token = os.environ.get(
+        "DOCKER_INFLUXDB_INIT_ADMIN_TOKEN", "my-super-secret-auth-token"
+    )
+    tsdb_org = os.environ.get("DOCKER_INFLUXDB_INIT_ORG", "my-org")
+    tsdb_debug = TypeAdapter(bool).validate_python(
+        os.environ.get("TSDB_DEBUG", "0")
+    )
+    tsdb_bucket = os.environ.get("DOCKER_INFLUXDB_INIT_BUCKET", "my-bucket")
     logger.info("Create worker [{}]", queue_name)
-    worker = Worker()
+    worker = Worker(
+        mq_host=mq_host,
+        tsdb_host=tsdb_host,
+        tsdb_token=tsdb_token,
+        tsdb_org=tsdb_org,
+        tsdb_bucket=tsdb_bucket,
+        tsdb_debug=tsdb_debug,
+    )
 
     logger.info("[MQ] start consuming [{}]", queue_name)
     worker.start_consuming(queue_name)
